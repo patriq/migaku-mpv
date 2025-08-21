b1e4287b2748e3633122629afaffcd8b3b861df5
Andre <patriqdesigns@gmail.com>
2025-08-21 22:10:16 +0200

    Changed play_url to become closer to the original from James Ross-Gowan

--- modules/play_url.lua
+++ modules/play_url.lua
@@ -1,9 +1,5 @@
 local utils = require('mp.utils')
 
-
-local os_name = nil;
-
-
 -- OS detection and get_clipboard_text adapterd from https://github.com/rossy/mpv-repl/blob/master/repl.lua
 --
 -- © 2016, James Ross-Gowan
@@ -12,29 +8,31 @@ local os_name = nil;
 -- purpose with or without fee is hereby granted, provided that the above
 -- copyright notice and this permission notice appear in all copies.
 
-local o = {}
--- Kind of a dumb way of detecting the platform but whatever
-if mp.get_property_native('options/vo-mmcss-profile', o) ~= o then
-	os_name = 'windows'
-elseif mp.get_property_native('options/cocoa-force-dedicated-gpu', o) ~= o then
-	os_name = 'macos'
-else
-	os_name = 'linux'
+function detect_platform()
+    local o = {}
+    -- Kind of a dumb way of detecting the platform but whatever
+    if mp.get_property_native('options/vo-mmcss-profile', o) ~= o then
+        return 'windows'
+    elseif mp.get_property_native('options/cocoa-force-dedicated-gpu', o) ~= o then
+        return 'macos'
+    end
+    return 'linux'
 end
 
+local platform = detect_platform()
 
 function get_clipboard_text()
-	if os_name == 'linux' then
+    if platform == 'linux' then
         local res = utils.subprocess({
-			args = { 'xclip', '-selection', 'clipboard', '-out' },
-			playback_only = false,
-		})
-		if not res.error then
-			return res.stdout
-		end
-	elseif os_name == 'windows' then
-		local res = utils.subprocess({
-			args = { 'powershell', '-NoProfile', '-Command', [[& {
+            args = { 'xclip', '-selection', 'clipboard', '-out' },
+            playback_only = false,
+        })
+        if not res.error then
+            return res.stdout
+        end
+    elseif platform == 'windows' then
+        local res = utils.subprocess({
+            args = { 'powershell', '-NoProfile', '-Command', [[& {
 				Trap {
 					Write-Error -ErrorRecord $_
 					Exit 1
@@ -50,23 +48,22 @@ function get_clipboard_text()
 				$u8clip = [System.Text.Encoding]::UTF8.GetBytes($clip)
 				[Console]::OpenStandardOutput().Write($u8clip, 0, $u8clip.Length)
 			}]] },
-			playback_only = false,
-		})
-		if not res.error then
-			return res.stdout
-		end
-	elseif os_name == 'macos' then
-		local res = utils.subprocess({
-			args = { 'pbpaste' },
-			playback_only = false,
-		})
-		if not res.error then
-			return res.stdout
-		end
-	end
-	return ''
+            playback_only = false,
+        })
+        if not res.error then
+            return res.stdout
+        end
+    elseif platform == 'macos' then
+        local res = utils.subprocess({
+            args = { 'pbpaste' },
+            playback_only = false,
+        })
+        if not res.error then
+            return res.stdout
+        end
+    end
+    return ''
 end
-
 
 function url_play(mode)
     local clipboard_text = get_clipboard_text()
@@ -80,13 +77,12 @@ function url_play(mode)
 end
 
 function url_play_replace()
-	url_play('replace')
+    url_play('replace')
 end
 
 function url_play_append()
-	url_play('append-play')
+    url_play('append-play')
 end
-
 
 mp.add_key_binding('y', 'url_play_replace', url_play_replace)
 mp.add_key_binding('Y', 'url_play_append', url_play_append)
